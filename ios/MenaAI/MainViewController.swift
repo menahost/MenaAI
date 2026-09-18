15ea8c6d710fb45efbb08b949cf3ce66a2af77e5
Mena Host <menahostltd@gmail.com>
2026-09-18 23:00:41 +0100

    Update MainViewController.swift

--- ios/MenaAI/MainViewController.swift
+++ ios/MenaAI/MainViewController.swift
@@ -1,22 +1,36 @@
 import UIKit
 import WebKit
-final class MainViewController:UIViewController,WKNavigationDelegate {
-    var webView:WKWebView!
-    override func viewDidLoad(){
+
+class MainViewController: UIViewController, WKNavigationDelegate {
+
+    private var webView: WKWebView!
+
+    override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor=.black
-        let c=WKWebViewConfiguration()
-        webView=WKWebView(frame:.zero,configuration:c)
-        webView.navigationDelegate=self
-        webView.translatesAutoresizingMaskIntoConstraints=false
+
+        view.backgroundColor = .black
+
+        let configuration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: configuration)
+
+        webView.navigationDelegate = self
+        webView.translatesAutoresizingMaskIntoConstraints = false
+
         view.addSubview(webView)
+
         NSLayoutConstraint.activate([
-            webView.topAnchor.constraint(equalTo:view.topAnchor),
-            webView.bottomAnchor.constraint(equalTo:view.bottomAnchor),
-            webView.leadingAnchor.constraint(equalTo:view.leadingAnchor),
-            webView.trailingAnchor.constraint(equalTo:view.trailingAnchor)
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
-        webView.load(URLRequest(url:URL(string:"https://ai.menahost.net/")!))
+
+        if let url = URL(string: "https://ai.menahost.net/") {
+            webView.load(URLRequest(url: url))
+        }
     }
-    override var preferredStatusBarStyle:UIStatusBarStyle{.lightContent}
+
+    override var prefersStatusBarHidden: Bool {
+        return false
+    }
 }
